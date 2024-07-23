b4206157b1f772313508534e10dc616d19ed73bc
Adityabaan Tripathy <adityabaantripathy@gmail.com>
2024-07-24 00:40:00 +0530

    Add files via upload

---
+++ Expense_Income_Tracker/src/expense_income_tracker/Expense_Income_Tracker.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expense_income_tracker;
+
+/**
+ *
+ * @author ADITYA
+ */
+public class Expense_Income_Tracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+        // TODO code application logic here
+    }
+    
+}
